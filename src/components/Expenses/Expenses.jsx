064934eb831757a4dd10fd41bf7245d136041fef
ZorageV <manjeetazim@mgial.com>
2023-06-27 05:18:33 +0530

    Finished

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpenseList from "./ExpenseList";
+import ExpenseChart from "./ExpenseChart";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState(2021);
@@ -15,6 +16,7 @@ const Expenses = (props) => {
     const filteredExpenses = props.expenseDetails.filter(filterHandler)
     return (
         <Card className="expenses">
+            <ExpenseChart expenses= {filteredExpenses}></ExpenseChart>
             <ExpensesFilter
                 selectedYear={filteredYear}
                 onChangeFilter={filterChangeHandler}
